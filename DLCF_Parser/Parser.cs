71f050a215d7d176c4817ff891284c7d8f9441e0
Jakub Pitner <jakub.pitner128@seznam.cz>
2023-07-29 15:11:20 +0200

    Added ParserTest

--- DLCF_Parser/Parser.cs
+++ DLCF_Parser/Parser.cs
@@ -1,56 +1,53 @@
-﻿using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
 
 namespace DLCF_Parser
 {
     public class Parser
     {
-        [DllImport("User32.dll", CharSet = CharSet.Unicode)]
-        public static extern int MessageBox(IntPtr h, string m, string c, int type);
-        public IDictionary<string, string> parseFile(string path) {
-            int status = checkForSpec(path);
-            string beforeEqual;
-            string afterEqual;
-            IDictionary<string, string> data = new Dictionary<string, string>();
-            switch (status)
+        public IDictionary<string, string> ParseFile(string path)
+        {
+            if (!File.Exists(path))
             {
-                case 0:
-                    string[] lines = File.ReadAllLines(path);
-                    foreach(string line in lines)
-                    {
-                        if (line.StartsWith('&'))
-                        {
-                            break;
-                        }
-                        beforeEqual = line.Split('=')[0];
-                        afterEqual = line.Substring(line.LastIndexOf('=') + 1);
-                        data.Add(beforeEqual, afterEqual);
-                    }
-                    break;
-                case 1:
-                    MessageBox((IntPtr)0, "Invalid file", "Invalid file", 0);
-                    break;
-                default:
-                    throw new Exception("Unspecified error code, if you see this, please report the incident");
+                throw new FileNotFoundException("File not found", path);
             }
+
+            if (!CheckForSpec(path))
+            {
+                throw new InvalidOperationException("Invalid file");
+            }
+
+            string[] lines = File.ReadAllLines(path);
+            Console.WriteLine($"Read {lines.Length} lines from the file.");
+            IDictionary<string, string> data = new Dictionary<string, string>();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i].Trim();
+
+                if (string.IsNullOrEmpty(line) || line.StartsWith("#"))
+                {
+                    continue;
+                }
+
+                string[] parts = line.Split(new char[] { '=' }, 2);
+                if (parts.Length == 2)
+                {
+                    string key = parts[0].Trim();
+                    string value = parts[1].Trim();
+
+                    data.Add(key, value);
+                }
+            }
+
             return data;
         }
 
-        private int checkForSpec(string file)
+        private bool CheckForSpec(string file)
         {
-            int statusCode;
             string[] lines = File.ReadAllLines(file);
-            if (lines[0] == "&DLFC 1.0 Private")
-            {
-                statusCode = 0;
-            } else
-            {
-                statusCode = 1;
-            }
-            return statusCode;
+            return (lines.Length > 0 && lines[0] == "&DLFC 1.0 Private");
         }
     }
 }
---
+++ ParserTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using DLCF_Parser;
+
+string filePath = "C:\\Users\\janpi\\Desktop\\test.dlfc";
+Parser parser = new Parser();
+
+try
+{
+    Console.WriteLine("Starting DLFC parsing...");
+    IDictionary<string, string> dlfcData = parser.ParseFile(filePath);
+    
+
+    // Access the parsed data
+    foreach (KeyValuePair<string, string> kvp in dlfcData)
+    {
+        Console.WriteLine("{0} : {1}", kvp.Key, kvp.Value);
+    }
+
+    // Continue accessing other key-value pairs as needed
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"File not found: {ex.Message}");
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine($"Invalid DLFC file: {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
